8c727978d45d5f20496a267e74f7b3ac771add49
AnnyFerreira <jacianeferreira29@hotmail.com>
2017-07-11 23:57:08 -0300

    HermioneFinal

---
+++ 2C/2-LivrosDaHermione/2-LivrosDaHermione/Livro.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_LivrosDaHermione
+{
+    class Livro
+    {
+        public string titulo;
+        public double qtdpaginas, pph;
+        public double DD; //dias até a devolução
+
+        public double Tempo(double pph, double qtdpaginas)
+        {
+            double totalT = qtdpaginas / pph;
+            return totalT;
+        }
+    }
+}
---
+++ 2C/2-LivrosDaHermione/2-LivrosDaHermione/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_LivrosDaHermione
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pph, DD, hpd, qtdpaginas, totalT = 0, totalPossivel = 0;
+            int n, i;
+            string titulo;
+
+
+            Console.WriteLine("Informe a quantidade de livros");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe a quantidade de páginas por hora");
+            pph = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Informe a quantidade de horas diárias");
+            hpd = Convert.ToDouble(Console.ReadLine());
+
+
+            Livro[] livros = new Livro[n];
+
+            for (i = 0; i < n; i++)
+            {
+                Livro livreto = new Livro();
+                Console.WriteLine("Informe o nome do livro");
+                livreto.titulo = Console.ReadLine();
+                Console.WriteLine("Informe a quantidade de paginas");
+                livreto.qtdpaginas = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Informe quantos dias até a devolução");
+                livreto.DD = Convert.ToDouble(Console.ReadLine());
+
+            }
+            
+               for (i = 0; i < n; i++)
+            {
+
+                if (livros[i].DD < (livros[i].pph / hpd))
+                {
+                    totalT = totalT + (livros[i].pph / hpd);
+                    Console.WriteLine("{0} é impossível de ser lido a tempo", livros[i].titulo);
+                }
+                else
+                {
+                    Console.WriteLine("{0} é possível de ser lido", livros[i].titulo);
+                    totalPossivel = totalPossivel + (livros[i].pph / hpd);
+                } 
+
+                
+            }
+            Console.WriteLine("Os Livros impossíveis de serem lidos completamente até a entrega, será de: {0} dias", totalT);
+            Console.WriteLine("Os livros possíveis de serem lidos completos até a entrega, será de: {0} dias", totalPossivel);
+        }
+    }
+}
+
