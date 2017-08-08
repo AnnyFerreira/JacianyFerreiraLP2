b7012b09fe91fc4df7fd25fc7ccc5245706e992b
AnnyFerreira <jacianeferreira29@hotmail.com>
2017-08-08 16:13:04 -0300

    CodigoMorseInicio

---
+++ 2C/4- CodigoMorse/4-CodigoMorse/4-CodigoMorse/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_CodigoMorse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Conversão de mensagem\n");
+            Console.WriteLine("1-codificar");
+            Console.WriteLine("2-decodificar");
+            
+            int m.texto(Console.ReadLine);
+           
+            
+        }
+    }
+}
---
+++ 2C/4- CodigoMorse/4-CodigoMorse/4-CodigoMorse/mensagem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_CodigoMorse
+{
+    class mensagem
+    {
+        private string texto { get; set; }
+    
+        public string Codificar ()
+        {
+
+        }
+        public string Decodificar()
+        {
+
+        }
+        public void Transmitir()
+        {
+
+        }
+    
+    }
+}
