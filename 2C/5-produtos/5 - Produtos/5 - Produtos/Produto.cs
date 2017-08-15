1c6185518efb458ae846b505d0adde7a802cdee2
AnnyFerreira <jacianeferreira29@hotmail.com>
2017-08-15 15:55:45 -0300

    produto1

---
+++ 2C/5-produtos/5 - Produtos/5 - Produtos/Produto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5___Produtos
+{
+    class Produto
+    {
+        private int id;
+        public int Id {
+                get { return id;}
+        }
+
+        public string nome {get;set;}
+        public double preco {get;set;}
+
+        private int quantidade;
+        public int Quantidade
+        {
+            get { return quantidade; }
+        }
+    }
+}
---
+++ 2C/5-produtos/5 - Produtos/5 - Produtos/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5___Produtos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto prod = new Produto();
+            prod.nome = "Colgate ";
+            prod.preco = 3.79;
+            
+        }
+    }
+}
