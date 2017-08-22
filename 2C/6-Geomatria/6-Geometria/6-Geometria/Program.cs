a4fdee0c8834c6058e6eb5b1f5bd5fb92e2d1020
AnnyFerreira <jacianeferreira29@hotmail.com>
2017-08-22 15:59:23 -0300

    triangulo

---
+++ 2C/6-Geomatria/6-Geometria/6-Geometria/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_Geometria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A = Convert.ToDouble(Console.ReadLine());
+            double B = Convert.ToDouble(Console.ReadLine());
+            double C = Convert.ToDouble(Console.ReadLine());
+            double cos = Convert.ToDouble(Console.ReadLine());
+        }
+    }
+}
---
+++ 2C/6-Geomatria/6-Geometria/6-Geometria/Triangulo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_Geometria
+{
+    class Triangulo
+    {
+        public double A {get; set;}
+        public double B {get; set;}
+        public double C {get; set;}
+        public double cos {get; set;}
+
+        public void area (double A,B)
+    {
+        C = (A*B) /2;
+        return  C; 
+    }
+
+        public double cos1 (double A , B, C,cos)
+
+        {
+            (A * A) = (B*B)+(C*C) - 2 * B * C * cos; 
+            return A 
+        }
+        
+    }
+}
